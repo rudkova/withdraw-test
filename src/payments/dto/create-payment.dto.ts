5bfe56516dee3ff5b93de855d90cab2b5261323a
Lucy Rudkova <lucy.rudkova@gmail.com>
2025-10-31 17:42:44 +0200

    validate CreatePaymentDto

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,4 +1,14 @@
+import { IsInt, IsNotEmpty, IsNumber, IsPositive, Max } from 'class-validator';
+
 export class CreatePaymentDto {
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
   userId: number;
+
+  @Max(10000)
+  @IsNumber({ allowInfinity: false, allowNaN: false })
+  @IsPositive()
+  @IsNotEmpty()
   amount: number;
 }
